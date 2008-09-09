2f1388b4be2cf28acabc6700af5cefe83408373c
Stig Thormodsrud <stig@io.vyatta.com>
2008-09-09 16:19:33 -0700

    - Change default to not block blacklists. - Delete "allow-category". - Add "block-category". - Add "local-ok" and "local-block".

--- scripts/vyatta-update-webproxy.pl
+++ scripts/vyatta-update-webproxy.pl
@@ -240,24 +240,46 @@ sub squidguard_get_constants {
     return $output;
 }
 
-sub squidguard_generate_block_site {
-    my @block_sites = @_;
+sub squidguard_generate_local {
+    my ($action, @local_sites) = @_;
 
-    my $db_dir = VyattaWebproxy::squidguard_get_blacklist_dir();
-    my $block_site = 'block-site';
-    my $dir = "$db_dir/$block_site";
+    my $db_dir       = VyattaWebproxy::squidguard_get_blacklist_dir();
+    my $local_action = "local-$action";
+    my $dir          = "$db_dir/$local_action";
 
-    if (scalar(@block_sites) <= 0) {
+    if (scalar(@local_sites) <= 0) {
 	system("rm -rf $dir") if -d $dir;
-	return undef;
+	return "";
     }
 
     system("mkdir $dir") if ! -d $dir;
     my $file = "$dir/domains";
     open(my $FD, ">", $file) or die "unable to open $file $!";
-    print $FD join("\n", @block_sites), "\n";
+    print $FD join("\n", @local_sites), "\n";
     close $FD;
-    return $block_site;
+    system("chown -R proxy.proxy $dir > /dev/null 2>&1");
+    squidguard_generate_db($local_action);
+    return $local_action;
+}
+
+sub squidguard_build_dest {
+    my ($category, $logging) = @_;
+
+    my $output = "";
+    my ($domains, $urls, $exps) =
+	VyattaWebproxy::squidguard_get_blacklist_domains_urls_exps($category);
+    if (!defined $domains and !defined $urls and !defined $exps) {
+	return "";
+    }
+    $output  = "dest $category {\n";
+    $output .= "\tdomainlist     $domains\n" if defined $domains;
+    $output .= "\turllist        $urls\n"    if defined $urls;
+    $output .= "\texpressionlist $exps\n"    if defined $exps;
+    if ($logging) {
+	$output .= "\tlog            $squidguard_blacklist_log\n";
+    }
+    $output .= "}\n\n";
+    return $output;
 }
 
 sub squidguard_get_values {
@@ -265,71 +287,65 @@ sub squidguard_get_values {
     my $config = new VyattaConfig;
 
     my $path = "service webproxy url-filtering squidguard";
- 
-    $config->setLevel("$path block-site");
-    my @block_sites = $config->returnValues();
-    squidguard_generate_block_site(@block_sites);
 
-    $config->setLevel("$path allow-category");
-    my @allow_category = $config->returnValues();
+    $config->setLevel("$path local-ok");
+    my @local_ok_sites = $config->returnValues();
+    my $local_ok = squidguard_generate_local('ok', @local_ok_sites);
+ 
+    $config->setLevel("$path local-block");
+    my @local_block_sites = $config->returnValues();
+    my $local_block       = squidguard_generate_local('block', 
+						      @local_block_sites);
+
+    $config->setLevel("$path block-category");
+    my @block_category = $config->returnValues();
+    my %is_block       = map { $_ => 1 } @block_category;    
 
     $config->setLevel("$path log");
     my @log_category = $config->returnValues();
     my %is_logged    = map { $_ => 1 } @log_category;    
 
-    my @blacklists = VyattaWebproxy::squidguard_get_blacklists();
+    my @blacklists   = VyattaWebproxy::squidguard_get_blacklists();
     my %is_blacklist = map { $_ => 1 } @blacklists;
     if (scalar(@blacklists) <= 0) {
 	print "Warning: no blacklists installed\n";
     }
-
-    my $ok = "";
-    if (scalar(@allow_category > 0)) {
-	$output .= "dest ok {\n";
-	foreach my $allow (@allow_category) {
-	    if (! defined $is_blacklist{$allow}) {
-		print "Unknown blacklist category [$allow]\n";
-		exit 1;
-	    }
-	    my ($domains, $urls, $exps) = 
-		VyattaWebproxy::squidguard_get_blacklist_domains_urls_exps(
-		    $allow);
-	    $output    .= "\tdomainlist     $domains\n" if defined $domains;
-	    $output    .= "\turllist        $urls\n"    if defined $urls;
-	    $output    .= "\texpressionlist $exps\n"    if defined $exps;
+    if (defined $is_block{all}) {
+	@block_category = ();
+	foreach my $category (@blacklists) {
+	    next if $category eq "local-ok";
+	    next if $category eq "local-block";
+	    push @block_category, $category;
 	}
-	$output .= "}\n\n";
-	$ok = "ok";
     }
-    
+
+    foreach my $category ($local_ok) {
+	next if $category eq "";
+	$output .= squidguard_build_dest($category, 0);
+    }
+
     my $acl_block = "";
-    foreach my $category (@blacklists) {
+    foreach my $category ($local_block, @block_category) {
+	next if $category eq "";
 	if (! defined $is_blacklist{$category}) {
 	    print "Unknown blacklist category [$category]\n";
 	    exit 1;
 	}
-	my ($domains, $urls, $exps) =
-	    VyattaWebproxy::squidguard_get_blacklist_domains_urls_exps(
-		$category);
-	$output    .= "dest $category {\n";
-	$output    .= "\tdomainlist     $domains\n" if defined $domains;
-	$output    .= "\turllist        $urls\n"    if defined $urls;
-	$output    .= "\texpressionlist $exps\n"    if defined $exps;
+	my $logging = 0;
 	if (defined $is_logged{all} or defined $is_logged{$category}) {
-	    $output    .= "\tlog            $squidguard_blacklist_log\n";
+	    $logging = 1;
 	}
-	$output    .= "}\n\n";
+	$output .= squidguard_build_dest($category, $logging);
 	$acl_block .= "!$category ";
     }
 
     $output .= "acl {\n";
     $output .= "\tdefault {\n";
-    $output .= "\t\tpass $ok !in-addr $acl_block all\n";
+    $output .= "\t\tpass $local_ok !in-addr $acl_block all\n";
 
     $config->setLevel($path);
     my $redirect_url = $config->returnValue("redirect-url");
     $redirect_url = $squidguard_redirect_def if ! defined $redirect_url;
-
     $output .= "\t\tredirect 302:$redirect_url\n\t}\n}\n";
 
     return $output;
@@ -360,6 +376,38 @@ sub squidguard_install_blacklist_def {
     return 0;
 }
 
+sub squidguard_generate_db {
+    my $category = shift;
+
+    my $db_dir   = VyattaWebproxy::squidguard_get_blacklist_dir();
+    my $tmp_conf = "/tmp/sg.conf.$$";
+    my $output   = "dbhome $db_dir\n";
+    $output     .= squidguard_build_dest($category, 0);
+    webproxy_write_file($tmp_conf, $output);
+
+    foreach my $type ('domains', 'urls') {
+	my $path = "$category/$type";
+	my $file = "$db_dir/$path";
+	if (-e $file) {
+	    my $file_db = "$file.db";
+	    if (! -e $file_db) {
+		#
+		# it appears that there is a bug in squidGuard that if
+		# the db file doesn't exist then running with -C leaves
+		# huge tmp files in /var/tmp.
+		#
+		system("touch $file.db");
+		system("chown -R proxy.proxy $file.db > /dev/null 2>&1");
+	    }
+	    my $wc = `cat $file| wc -l`; chomp $wc;
+	    print "Building DB for [$path] - $wc entries\n";
+	    my $cmd = "\"squidGuard -c $tmp_conf -C $path\"";
+	    system("su - proxy -c $cmd > /dev/null 2>&1");
+	}
+    }
+    system("rm $tmp_conf");
+}
+
 sub squidguard_update_blacklist {
 
     my @blacklists = VyattaWebproxy::squidguard_get_blacklists();
@@ -379,43 +427,10 @@ sub squidguard_update_blacklist {
     system("chmod 2770 $db_dir >/dev/null 2>&1");
 
     #
-    # generate temporary config
+    # generate temporary config for each category & generate DB
     #
-    my $tmp_conf = "/tmp/sg.conf.$$";
     foreach my $category (@blacklists) {
-	my $output;
-	$output  = "dbhome $db_dir\n";
-	$output .= "dest block {\n";
-	my ($domains, $urls, $exps) =
-	    VyattaWebproxy::squidguard_get_blacklist_domains_urls_exps(
-		$category);
-	$output .= "\tdomainlist     $domains\n" if defined $domains;
-	$output .= "\turllist        $urls\n"    if defined $urls;
-	$output .= "\texpressionlist $exps\n"    if defined $exps;
-	$output .= "}\n\n";
-	webproxy_write_file($tmp_conf, $output);
-    
-	foreach my $type ("domains", "urls") {
-	    my $path = "$category/$type";
-	    my $file = "$db_dir/$path";
-	    if (-e $file) {
-		my $file_db = "$file.db";
-		if (! -e $file_db) {
-		    #
-		    # it appears that there is a bug in squidGuard that if
-		    # the db file doesn't exist then running with -C leaves
-		    # huge tmp files in /var/tmp.
-		    #
-		    system("touch $file.db");
-		    system("chown -R proxy.proxy $file.db > /dev/null 2>&1");
-		}
-		my $wc = `cat $file| wc -l`; chomp $wc;
-		print "Building DB for [$path] - $wc entries\n";
-		my $cmd = "\"squidGuard -c $tmp_conf -C $path\"";
-		system("su - proxy -c $cmd > /dev/null 2>&1");
-	    }
-	}
-	system("rm $tmp_conf");
+	squidguard_generate_db($category);
     }
 }
 
